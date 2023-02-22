bf52ca47f331a14a63838c2b08c89599f6706a87
austris <austris.u@gmail.com>
2023-02-22 09:28:22 +0200

    Create prog.m

---
+++ plain-native/prog.m
@@ -0,0 +1,11 @@
+#include <Foundation/Foundation.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include <objc/runtime.h>
+#import <UIKit/UIKit.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
